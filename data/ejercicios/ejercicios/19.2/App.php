4c8fdccfc5708a195f6fbc47be8d299f0b6f9efb
guillermo rodriguez <guillerhuguet@gmail.com>
2022-11-03 15:05:55 +0000

    col

---
+++ data/ejercicios/ejercicios/19.2/App.php
@@ -0,0 +1,12 @@
+<?php
+
+class App
+{
+
+    function colorrojo(){
+        echo "<style type'text/css'>
+                body{
+                    background-color: red;
+                 } </style>";
+    }
+}
